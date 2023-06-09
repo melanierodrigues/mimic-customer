45ec57c785cca099f93f2d3b3a36be0dedbf3326
melanie.rodrigues <hello@melanierodrigues.com>
2023-06-09 14:48:34 +0100

    added onError load image

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,17 @@
 import '../Card/Card.scss'
 
+const BrokenImage = "https://infra-cloudfront-talkdeskcom.svc.talkdeskapp.com/talkdesk_com/2021/06/16035537/broken-image-3_2.png";
+
 const Card = (props) => {
+  const imageOnError = (event) => {
+    event.currentTarget.src = BrokenImage;
+    event.currentTarget.className = "error";
+  };
+
   return (
     <a className="card-wrapper" href={`https://www.talkdesk.com/${props.link.url}`} target={props.link.target}>
       <div className="card-wrapper image-container">
-        <img src={props.image} alt={props.imageAlt} />
+        <img src={props.image} alt={props.imageAlt} onError={imageOnError}/>
       </div>
       <a className="text-container" href={`https://www.talkdesk.com/${props.link.url}`} target={props.link.target}>
         <span className="label">{props.label}</span>
--- src/components/EmptyState/EmptyState.js
+++ src/components/EmptyState/EmptyState.js
@@ -1,6 +1,13 @@
 import '../EmptyState/EmptyState.scss'
 
+const BrokenImage = "https://infra-cloudfront-talkdeskcom.svc.talkdeskapp.com/talkdesk_com/2021/06/16035537/broken-image-3_2.png";
+
 const EmptyState = (props) => {
+  const imageOnError = (event) => {
+    event.currentTarget.src = BrokenImage;
+    event.currentTarget.className = "error";
+  };
+
   return (
     <div className="empty-state-container">
       <div className="text">
@@ -9,7 +16,7 @@ const EmptyState = (props) => {
         <p>Please, try different <strong>search</strong> words.</p>
       </div>
       <div className="image">
-        <img src={props.image} alt={props.imageAlt} onerror/>
+        <img src={props.image} alt={props.imageAlt} onError={imageOnError}/>
       </div>
     </div>
   )
